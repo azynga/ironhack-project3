67a7844e163395ddb518f95472d279490ec28ce0
Aljoscha Zynga <aljoscha.zynga@gmail.com>
2022-05-19 22:30:40 +0200

    add proxy, change localhost urls

--- server/client/src/services/auth-service.js
+++ server/client/src/services/auth-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const service = axios.create({
-    baseURL: 'http://localhost:5005/api/auth',
+    baseURL: '/api/auth',
     withCredentials: true,
 });
 
--- server/client/src/services/user-service.js
+++ server/client/src/services/user-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const service = axios.create({
-    baseURL: 'http://localhost:5005/api',
+    baseURL: '/api',
 });
 
 const saveNotification = (userId, notification) => {
