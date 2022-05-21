5540033ab6f815424816fe4415cc7b9f0e3373ad
Aljoscha Zynga <aljoscha.zynga@gmail.com>
2022-05-21 02:33:32 +0200

    fix some urgent styling issues, add error messages to image upload and login

--- server/config/index.js
+++ server/config/index.js
@@ -47,7 +47,7 @@ module.exports = (app) => {
     // In development environment the app logs
     app.use(logger('dev'));
     // To have access to `body` property in the request
-    app.use(express.json());
+    app.use(express.json({ limit: '10mb' }));
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
 
